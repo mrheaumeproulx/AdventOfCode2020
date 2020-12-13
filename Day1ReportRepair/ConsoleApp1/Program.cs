ff9bc5f0a04424637f8aca4d192b188029825174
Maxime Rheaume-Proulx <maxime.r.proulx@gmail.com>
2020-12-13 10:53:07 -0500

    Day 1 - part 2

--- Day1ReportRepair/ConsoleApp1/Program.cs
+++ Day1ReportRepair/ConsoleApp1/Program.cs
@@ -10,21 +10,38 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Expenses reports 2020");
-
-            (bool found, int entry1, int entry2) = GetEntries();
-
-            if (found)
+            var invalidTries = 0;
+            while (true)
             {
-                Console.WriteLine($"Two entries found: {entry1} + {entry2} = 2020");
-                Console.WriteLine($"The answer is {entry1 * entry2}");
+                Console.WriteLine("Choose a report, 1 for two entries or 2 for three entries");
+                var reportChosen = Console.ReadKey();
+                Console.WriteLine();
+                if (reportChosen.Key == ConsoleKey.D1)
+                {
+                    GetEntries1();
+                    break;
+                }
+                else if (reportChosen.Key == ConsoleKey.D2)
+                {
+                    GetEntries2();
+                    break;
+                }
+                else
+                {
+                    invalidTries++;
+                    if (invalidTries == 2)
+                    {
+                        Console.WriteLine("You are a lost cause, closing this program for you");
+                        break;
+                    }
+                    Console.WriteLine("Invalid key, please try again");
+                }
             }
-            else
-                Console.WriteLine("There are no entries that sums to 2020");
 
             Console.ReadKey();
         }
 
-        public static (bool found, int entry1, int entry2) GetEntries()
+        public static void GetEntries1()
         {
             var entries = GetExpenses();
 
@@ -33,17 +50,37 @@ namespace ConsoleApp1
                 var entry = entries.ElementAt(i);
 
                 if (entries.Any(e => e == (2020 - entry)))
-                    return (true, entry, entries.First(e => e == (2020 - entry)));
+                {
+                    var match = entries.First(e => e == (2020 - entry));
+                    Console.WriteLine($"Two entries found: {entry} + {match} = 2020");
+                    Console.WriteLine($"The answer is {entry * match}");
+                }
             }
+            Console.WriteLine("There are no entries that sums to 2020");
+        }
 
-            return (false, 0, 0);
+        private static void GetEntries2()
+        {
+            var entries = GetExpenses().ToArray();
+
+            for (int i = 0; i < entries.Length - 2; i++)
+            {
+                for (int j = i + 1; j < entries.Length - 1; j++)
+                {
+                    for (int k = j + 1; k < entries.Length; k++)
+                    {
+                        if (entries[i] + entries[j] + entries[k] == 2020)
+                        {
+                            Console.WriteLine($"Two entries found: {entries[i]} + {entries[j]} + {entries[k]} = 2020");
+                            Console.WriteLine($"The answer is {entries[i] * entries[j] * entries[k]}");
+                            break;
+                        }
+                    }
+                }
+            }
         }
 
         private static IEnumerable<int> GetExpenses()
-        {
-            var lines = File.ReadAllLines("Expenses.txt");
-
-            return lines.Select(line => int.Parse(line));
-        }
+            => File.ReadAllLines("Expenses.txt").Select(line => int.Parse(line));
     }
 }
